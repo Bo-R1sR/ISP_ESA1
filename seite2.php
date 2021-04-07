44a793f5bfac169b970d73fae65c3cb54bea1603
Boris Roßbach <boris.rossbach@gmx.de>
2021-04-07 10:19:03 +0200

    initial commit

---
+++ seite2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="Kathrin Hapke, Boris Roßbach" name="author">
+    <link href="style.css" rel="stylesheet">
+    <title>Seite2</title>
+</head>
+
+<body>
+<header></header>
+<nav>
+    <a href="index.php">Link zur Startseite</a>
+    <a href="seite2.php">Link zur 2. Seite</a>
+</nav>
+<section>
+    <h1>Dies ist Seite 2</h1>
+    <!-- entnommen von  https://pixabay.com/de/photos/code-codierung-web-entwicklung-944499/  -->
+    <img alt="Code Beispiel" src="code-944499_640.jpg"/>
+</section>
+<footer></footer>
+</body>
+</html>
+
+<script src="index.js"></script>
